55432193501b602062792770ffc404c9de39f5c4
Ahmad Mostafa <a.m_eg@yahoo.com>
2022-03-01 11:20:50 +0200

    Update FirstWebApp.py

--- FirstWebApp.py
+++ FirstWebApp.py
@@ -1,13 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  1 10:06:56 2022
-
-@author: Ahmad
-"""
-
 import yfinance as yf
 import streamlit as st
 
 st.write("""
-         
-         #Hi """)
+# Simple Stock Price App
+Shown are the stock **closing price** and ***volume*** of Google!
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
